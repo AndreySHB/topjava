41b4aa0103f9c89d9ea6232e024ddf675aa837a4
deerhunter9136 <deerhunter9136@gmail.com>
2022-02-23 17:23:18 +0300

    HWO3 after 3nd review

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -41,7 +41,7 @@ public class MealServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.setCharacterEncoding("UTF-8");
         Meal meal = new Meal(
-                LocalDateTime.parse(request.getParameter("date")),
+                LocalDateTime.parse(request.getParameter("dateTime")),
                 request.getParameter("description"),
                 Integer.parseInt(request.getParameter("calories")));
 
