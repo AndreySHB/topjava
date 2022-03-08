a66cc2022fd4b65968d15a13c7b46f409a61b653
deerhunter9136 <deerhunter9136@gmail.com>
2022-03-09 01:21:05 +0300

    HW05 base done after review intermediate results

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
@@ -1,6 +1,5 @@
 package ru.javawebinar.topjava.repository.datajpa;
 
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -14,11 +13,22 @@ import java.util.List;
 @Transactional(readOnly = true)
 public interface CrudMealRepository extends JpaRepository<Meal, Integer> {
 
-    int deleteMealById(int id);
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM Meal m WHERE m.id=:id AND m.user.id=:userId")
+    int delete(@Param("id") int id, @Param("userId") int userId);
 
-    List<Meal> findAllByUserId(int userId, Sort sort);
+    @Query("SELECT m FROM Meal m WHERE m.user.id=:userId ORDER BY m.dateTime DESC")
+    List<Meal> findAllByUserId(@Param("userId") int userId);
 
-    List<Meal> findAllByUserIdAndDateTimeBetween(int userId, LocalDateTime startDateTime, LocalDateTime endDateTime, Sort sortDateDesc);
+    @Query("""
+            SELECT m FROM Meal m
+            WHERE m.user.id=:userId AND m.dateTime >= :startDateTime AND m.dateTime < :endDateTime ORDER BY m.dateTime DESC
+             """)
+    List<Meal> findAllByUserIdAndDateTimeBetween(@Param("userId") int userId,
+                                                 @Param("startDateTime") LocalDateTime startDateTime,
+                                                 @Param("endDateTime") LocalDateTime endDateTime);
 
-    Meal getMealByIdAndUserId(int id, int userId);
+    @Query("SELECT m FROM Meal m WHERE m.id=:id AND m.user.id=:userId")
+    Meal getByIdAndUserId(@Param("id") int id, @Param("userId") int userId);
 }
--- src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepository.java
@@ -2,6 +2,7 @@ package ru.javawebinar.topjava.repository.datajpa;
 
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 
@@ -22,36 +23,32 @@ public class DataJpaMealRepository implements MealRepository {
     }
 
     @Override
+    @Transactional
     public Meal save(Meal meal, int userId) {
         meal.setUser(crudUserRepository.getById(userId));
-        if (meal.isNew()) {
+        if (meal.isNew() || crudRepository.getByIdAndUserId(meal.id(), userId) != null) {
             return crudRepository.save(meal);
         }
-        return (crudRepository.getMealByIdAndUserId(meal.id(), userId) != null) ? crudRepository.save(meal) : null;
+        return null;
     }
 
     @Override
     public boolean delete(int id, int userId) {
-        Meal meal = crudRepository.getMealByIdAndUserId(id, userId);
-        if (meal != null) {
-            crudRepository.delete(meal);
-            return true;
-        }
-        return false;
+        return crudRepository.delete(id, userId) != 0;
     }
 
     @Override
     public Meal get(int id, int userId) {
-        return crudRepository.getMealByIdAndUserId(id, userId);
+        return crudRepository.getByIdAndUserId(id, userId);
     }
 
     @Override
     public List<Meal> getAll(int userId) {
-        return crudRepository.findAllByUserId(userId, SORT_DATE_DESC);
+        return crudRepository.findAllByUserId(userId);
     }
 
     @Override
     public List<Meal> getBetweenHalfOpen(LocalDateTime startDateTime, LocalDateTime endDateTime, int userId) {
-        return crudRepository.findAllByUserIdAndDateTimeBetween(userId, startDateTime, endDateTime.minusNanos(1000), SORT_DATE_DESC);
+        return crudRepository.findAllByUserIdAndDateTimeBetween(userId, startDateTime, endDateTime);
     }
 }
